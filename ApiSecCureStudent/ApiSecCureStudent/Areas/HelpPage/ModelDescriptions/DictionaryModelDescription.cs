180dda0497b3e378fc8595df78701158766ae446
adrianasalmon <a2019116341@estudiantes.upsa.edu.bo>
2020-08-29 09:31:05 -0400

    Proyecto inicial con Acoount

---
+++ ApiSecCureStudent/ApiSecCureStudent/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiSecCureStudent.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ApiSecCureStudent/ApiSecCureStudent/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiSecCureStudent.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
